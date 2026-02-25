d7ed7bf46a4574437c4d46f4f0466e94a90a0b5f
BANKIM27520 <BANKIM.27520@stu.upes.ac.in>
2026-02-25 22:03:31 +0530

    DSA PROGRAM

--- ep16-1.cpp
+++ ep16-1.cpp
@@ -11,19 +11,17 @@ Output:
 int main() {
     int n;
     int arr[100];
-    int freq[1001] = {0}; // Assuming values 0-1000
+    int freq[1001] = {0}; 
     
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
         freq[arr[i]]++;
     }
-    
-    // Print in order of first appearance
     for (int i = 0; i < n; i++) {
         if (freq[arr[i]] > 0) {
             printf("%d:%d ", arr[i], freq[arr[i]]);
-            freq[arr[i]] = 0; // Mark as printed
+            freq[arr[i]] = 0; 
         }
     }
     printf("\n");
