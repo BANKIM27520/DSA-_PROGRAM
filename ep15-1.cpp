be945bb33c6397aee098f1bc74b0ff7f5d2959eb
BANKIM27520 <BANKIM.27520@stu.upes.ac.in>
2026-02-25 21:54:40 +0530

    DSA PROGRAM

---
+++ ep15-1.cpp
@@ -0,0 +1,30 @@
+/*Given a matrix, calculate the sum of its primary diagonal elements. The primary diagonal consists of elements where row index equals column index.
+
+Input:
+- First line: two integers m and n
+- Next m lines: n integers each
+
+Output:
+- Print the sum of the primary diagonal elements
+*/
+#include <stdio.h>
+int main() {
+    int m, n;
+    int mat[100][100];
+    
+    scanf("%d %d", &m, &n);
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    
+    int sum = 0;
+    for (int i = 0; i < m && i < n; i++) {
+        sum += mat[i][i];
+    }
+    
+    printf("%d\n", sum);
+    return 0;
+}
