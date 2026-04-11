7adc7a3ef1c335400423426b4f010b81d431c21f
BANKIM27520 <BANKIM.27520@stu.upes.ac.in>
2026-04-11 10:00:00 +0530

    DSA PROGRAM

---
+++ ep49-1.c
@@ -0,0 +1,65 @@
+/*Problem: BST Insert
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications*/
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+int countLeaves(struct Node* root) {
+    if (root == NULL)
+        return 0;
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+    return countLeaves(root->left) + countLeaves(root->right);
+}
+int main() {
+    int N;
+    scanf("%d", &N);
+    int* levelOrder = (int*)malloc(N * sizeof(int));
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &levelOrder[i]);
+    }
+    struct Node* root = NULL;
+    struct Node** queue = (struct Node**)malloc(N * sizeof(struct Node*));
+    int front = 0, rear = 0;
+    for (int i = 0; i < N; i++) {
+        if (levelOrder[i] != -1) {
+            struct Node* newNode = createNode(levelOrder[i]);
+            if (root == NULL) {
+                root = newNode;
+            } else {
+                if (queue[front]->left == NULL) {
+                    queue[front]->left = newNode;
+                } else {
+                    queue[front]->right = newNode;
+                    front++;
+                }
+            }
+            queue[rear++] = newNode;
+        } else {
+            if (front < rear) {
+                front++;
+            }
+        }
+    }
+    printf("Number of leaf nodes: %d\n", countLeaves(root));
+    free(levelOrder);
+    free(queue);
+    return 0;
+}
