15bdd16167333451e79e35a0f2fd07d5c29f60ec
BANKIM27520 <BANKIM.27520@stu.upes.ac.in>
2026-04-29 10:00:00 +0530

    DSA program

---
+++ ep66.c
@@ -0,0 +1,74 @@
+/*Problem: Detect cycle in directed graph using DFS and recursion stack.
+
+Output:
+- YES if cycle exists*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+// adjacency list representation
+typedef struct Node {
+    int vertex;
+    struct Node* next;
+} Node;
+Node* createNode(int v) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+void addEdge(Node* adj[], int u, int v) {
+    Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+}
+bool DFSUtil(int v, bool visited[], bool recStack[], Node* adj[]) {
+    visited[v] = true;
+    recStack[v] = true;
+
+    Node* temp = adj[v];
+    while (temp != NULL) {
+        if (!visited[temp->vertex]) {
+            if (DFSUtil(temp->vertex, visited, recStack, adj)) return true;
+        } else if (recStack[temp->vertex]) {
+            return true; // cycle detected
+        }
+        temp = temp->next;
+    }
+    recStack[v] = false; // remove from recursion stack
+    return false;
+}
+bool hasCycle(int n, Node* adj[]) {
+    bool visited[n+1];
+    bool recStack[n+1];
+    for (int i = 0; i <= n; i++) {
+        visited[i] = false;
+        recStack[i] = false;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            if (DFSUtil(i, visited, recStack, adj)) return true;
+        }
+    }
+    return false;
+}
+int main() {
+    int n, e, u, v;
+    scanf("%d", &n);   // number of vertices
+    scanf("%d", &e);   // number of edges
+
+    Node* adj[n+1];
+    for (int i = 0; i <= n; i++) adj[i] = NULL;
+
+    for (int i = 0; i < e; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(adj, u, v); // directed graph
+    }
+
+    if (hasCycle(n, adj)) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    return 0;
+}
